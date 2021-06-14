795fba981314a564f34b797b4450fd39bd307364
codebyBarni <85904358+codebyBarni@users.noreply.github.com>
2021-06-14 22:24:52 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+const config = require('./config.json');
+const token = require('./config.json');
+
+bot.on("ready", ready => {
+    console.log(`Sikeresen bejelentkeztem! (${client.user.username})`)
+    client.user.setActivity(`Prefixem: ${config.prefix}`, {type: WATCHING})
+});
+
+client.login(token);
